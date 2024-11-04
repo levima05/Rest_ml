5b716d71d0f1b73b41c9d9bddf63d865e71e92ce
Petrik Diak <diak@petrik.hu>
2024-11-04 14:25:22 +0100

    Commit

---
+++ src/books/dto/update-book.dto.ts
@@ -0,0 +1,10 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateBookDto } from './create-book.dto';
+
+export class UpdateBookDto extends PartialType(CreateBookDto) {
+  title?: string;
+  author?: string;
+  isbn?: string;
+  publishYear?: string;
+  reserved?: string;
+}
